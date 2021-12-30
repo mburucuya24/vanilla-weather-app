0a195c422f33a115aafc66b500624576dca80a08
mburucuya24 <92304084+mburucuya24@users.noreply.github.com>
2021-12-30 16:46:54 -0500

    Added getForecast function to get the coordinates

--- src/app.js
+++ src/app.js
@@ -60,7 +60,14 @@ function search(city) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
-function displayForecast() {
+function getForecast(coordinates) {
+  let apiKey = "fa76215f0cf93568bc4f8d5fad72485f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -114,6 +121,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function displayFahrenheitTemperature(event) {
@@ -164,4 +173,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Warsaw");
-displayForecast();
